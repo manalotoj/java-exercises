4ca478b07bc5c177cbca0cd650e0a058084b59dc
John Manaloto <manaloto@macbook-pro.lan>
2020-10-12 21:09:19 -0700

    implement console app

--- garden/garden/src/main/java/garden/PA5Main.java
+++ garden/garden/src/main/java/garden/PA5Main.java
@@ -1,13 +1,145 @@
 package garden;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.javatuples.Pair;
+
 public class PA5Main 
 {
-    public static void main( String[] args )
-    {
+    static Garden garden;
+    static int rows;
+    static int cols;
+
+    public static void main( String[] args ) {
+        if (args.length == 0) return;
+        
+        String filePath = args[0];
+
+        BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(filePath));
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+                line = reader.readLine();
+                if (line.contains("rows")) {
+                    createGarden(line, reader.readLine());
+                } else if(line.equals("")) {
+                    // do nothing
+                } else {
+                    processGardenCommand(line);
+                }
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+
+    private static void createGarden(String rowsLine, String colsLine) {
+        rows =  Integer.parseInt(rowsLine.split(":")[1]);
+        cols = Integer.parseInt(colsLine.split(":")[1]);
+        garden = new Garden(rows, cols);
+    }
+
+    private static void processGardenCommand(String line) {
+        String[] lineParts = line.split(" ");
+        String cmd = lineParts[0];
+
+        switch(cmd) {
+            case "PLANT":
+                Pair<Integer, Integer> coords = parseCoordinates(lineParts[1]);
+                garden.plant(coords.getValue0(), coords.getValue1(), lineParts[2]);
+                break;
+
+            case "GROW":
+                processGrow(lineParts);
+                break;
+
+            case "harvest":
+                processHarvest(lineParts);            
+                break;
+
+            case "cut":
+                processCut(lineParts);
+                break;
+
+            case "pick":
+                processPick(lineParts);
+                break;
+
+            case "print":
+                garden.print();
+                break;
+        }
+    }
+
+    private static void processPick(String[] lineParts) {
+        if (lineParts.length == 1) {
+            garden.pick();
+        } else {
+            Pair<Integer, Integer> coords = parseCoordinates(lineParts[1]);
+            garden.pick(coords.getValue0(), coords.getValue1());
+        }
+    }
+
+    private static void processCut(String[] lineParts) {
+        if (lineParts.length == 1) {
+            garden.cut();
+        } else {
+            Pair<Integer, Integer> coords = parseCoordinates(lineParts[1]);
+            garden.cut(coords.getValue0(), coords.getValue1());
+        }
+    }
+
+    private static void processHarvest(String[] lineParts) {
+        if (lineParts.length == 1) {
+            garden.harvest();
+        } else {
+            Pair<Integer, Integer> coords = parseCoordinates(lineParts[1]);
+            garden.harvest(coords.getValue0(), coords.getValue1());
+        }
+    }
+
+    private static void processGrow(String[] lineParts) {
+        switch(lineParts.length) {
+            case 1:
+                garden.grow();
+                break;
+
+            case 2:
+                Integer amount = Integer.parseInt(lineParts[1]);
+                garden.grow(amount);
+                break;
+
+            case 3:
+                Integer amount1 = Integer.parseInt(lineParts[1]);
+                if (lineParts[3].contains("(")) {
+                    // grow 1 (x,y)
+                    Pair<Integer, Integer> coords = parseCoordinates(lineParts[2]);
+                    garden.grow(amount1, coords.getValue0(), coords.getValue1());
+                } else {
+                    // grow 1 plantType
+                    String plantType = lineParts[2];
+                    garden.growPlantType(amount1, plantType);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private static Pair<Integer, Integer> parseCoordinates(String coords) {
+        coords = coords.replace("(", "").replace(")", "");
+        String[] coorsParts = coords.split(",");
+        return new Pair<>(Integer.parseInt(coorsParts[0]), Integer.parseInt(coorsParts[1]));
+    }
+
+    private static void RunTests() {
         System.out.println( "Hello World!" );
         Garden g = new Garden(2, 1);
         System.out.println("Print empty 2x1 garden:");
